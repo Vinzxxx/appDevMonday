d76bb257a8eda401e3af22198ee587950f124088
Vinzxxx <mgcodilan@paterostechnologicalcollege.edu.ph>
2025-04-13 20:58:41 +0800

    Create Codilan.java
    Added Codilan.java with age calculation

---
+++ Codilan.java
@@ -0,0 +1,8 @@
+public class Codilan {
+    public static void display() {
+        int age = 21; // Change this to your actual age if you want
+        System.out.println("Hello! This is Codilan contributing to the project.");
+        System.out.println("My current age is: " + age);
+        System.out.println("Next year, I will be: " + (age + 1));
+    }
+}
