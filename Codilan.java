651272e83db648490881722a075595effdcf1699
Vinzxxx <mgcodilan@paterostechnologicalcollege.edu.ph>
2025-04-13 21:24:49 +0800

    Create Codilan.java

---
+++ Codilan.java
@@ -0,0 +1,8 @@
+public class Codilan {
+    public static void display() {
+        int age = 20; // Replace this with your age
+        System.out.println("Hello! This is Codilan contributing to the project.");
+        System.out.println("My current age is: " + age);
+        System.out.println("Next year, I will be: " + (age + 1));
+    }
+}
